3e6dbd4a8d08c5fbe986f43f357ef275d3e85866
datahoundz <jscotland@cox.net>
2018-03-03 18:10:05 -0700

    Modified histogram layout w/ geo layer

--- 02_stat_analysis.R
+++ 02_stat_analysis.R
@@ -12,17 +12,59 @@ options(scipen = 999, digits = 3)
 
 # =======================================================================
 # 
-# Statistical Analysis - Guns & Ammo Rank vs Giffords Law, Death Rank
+# Statistical Analysis - Ranking Data: Guns & Ammo vs Giffords Law Center
 # 
 # =======================================================================
+
+
+# Check distribution of Giffords Law Score (letter grade converted to numeric 0 to 4)
+giff_grd_df %>%
+  ggplot(aes(x = law_score)) +
+  geom_histogram() +
+  facet_grid(. ~ year)
+
+# Distribution is heavily right skewed with half of all states receiving a score of 0 or F.
+
+# Check for movement by adding geographical data layer to plot
+giff_grd_df %>%
+  left_join(regions_df, by = "state") %>%
+  ggplot(aes(x = law_grd, y = death_rnk, label = usps_st, color = region)) +
+  geom_text() +
+  facet_grid(. ~ year)
+
+# Check internal correlation between Giffords Law Rank and Death Rank (death rank reversed for same good to bad scale)
+giff_grd_df %>%
+  ggplot(aes(x = law_rnk, y = death_rnk)) +
+  geom_point() +
+  stat_smooth(method = "lm", se = FALSE)
+
+# Add geographical labeling element to identify outliers, facet by year
+giff_grd_df %>%
+  left_join(regions_df, by = "state") %>%
+  ggplot(aes(x = law_rnk, y = death_rnk, label = usps_st)) +
+  facet_wrap(~ year) +
+  geom_text() +
+  stat_smooth(method = "lm", se = FALSE)
+
+# Add regional facet to view correlation in greater detail
+
+# Add geographical labeling element to identify outliers, facet by year
+giff_grd_df %>%
+  left_join(regions_df, by = "state") %>%
+  ggplot(aes(x = law_rnk, y = death_rnk, label = usps_st, color = region)) +
+  facet_grid(region ~ year) +
+  geom_text() +
+  stat_smooth(method = "lm", se = FALSE)
+
+
 
 gun_ammo_df %>%
   filter(year == 2015) %>%
   left_join(giff_grd_df, by = join_key) %>%
   left_join(regions_df, by = "state") %>%
-  ggplot(aes(x = gun_ammo_rnk, y = law_rnk, label = usps_st)) +
+  ggplot(aes(x = gun_ammo_rnk, y = law_rnk, label = usps_st, color = region)) +
   geom_text(position = "jitter") +
-  stat_smooth(method = "lm", se = TRUE)
+  stat_smooth(method = "lm", se = FALSE, colour = "blue")
 
 ga_gf_mod <- gun_ammo_df %>%
   filter(year == 2015) %>%
@@ -35,10 +77,9 @@ gun_ammo_df %>%
   filter(year == 2015) %>%
   left_join(giff_grd_df, by = join_key) %>%
   left_join(regions_df, by = "state") %>%
-  ggplot(aes(x = gun_ammo_rnk, y = death_rnk, label = usps_st)) +
+  ggplot(aes(x = gun_ammo_rnk, y = death_rnk, label = usps_st, color = region)) +
   geom_text(position = "jitter") +
-  stat_smooth(method = "lm", se = FALSE) +
-  aes(color = region)
+  stat_smooth(method = "lm", se = FALSE, colour = "blue")
 
-mod2 <- lm(death_rnk ~ gun_ammo_rnk, mod1_df)
+mod2 <- lm(death_rnk ~ gun_ammo_rnk, ga_gf_mod)
 summary(mod2) 
