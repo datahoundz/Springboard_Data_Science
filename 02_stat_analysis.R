9a71a09421718ece98390b487bd36a8bf3ddbe12
datahoundz <jscotland@cox.net>
2018-03-04 16:49:15 -0700

    Plotting ownership and suicide rates plus

--- 02_stat_analysis.R
+++ 02_stat_analysis.R
@@ -6,13 +6,14 @@ library(dplyr)
 library(purrr)
 library(ggplot2)
 library(lubridate)
+library(broom)
 
 # Set options to limit sci notation and decimal places
 options(scipen = 999, digits = 3)
 
 # =======================================================================
 # 
-# Statistical Analysis - Ranking Data: Guns & Ammo vs Giffords Law Center
+# Statistical Analysis - Giffords Law Center Data
 # 
 # =======================================================================
 
@@ -23,8 +24,15 @@ giff_grd_df %>%
   geom_histogram() +
   scale_x_reverse() +
   facet_grid(. ~ year)
-
 # Distribution is heavily left skewed with half of all states receiving a score of 0 or F.
+
+
+# Check summary statistics by year
+giff_grd_df %>%
+  group_by(year) %>%
+  summarize(N = n(), Min = min(law_score), Max = max(law_score), AvgScore = mean(law_score), 
+            Median = median(law_score), IQR = IQR(law_score), SD = sd(law_score))
+# Zero scores clearly dominate distribution statistics w/ two median scores of 0
 
 # Add geographical data layer w/ state labels to plot, y-axis = death_rnk
 giff_grd_df %>%
@@ -33,6 +41,19 @@ giff_grd_df %>%
   ggplot(aes(x = law_grd, y = death_rnk, label = usps_st, color = region)) +
   geom_text() +
   facet_grid(. ~ year)
+# Modified histogram highlights F scores and worse Death Rank dominated by South and West.
+# Of the 20 worst death rankings, between 18 and 20 had a law grade of F over three years.
+
+# Check summary statistics by year, adding region
+giff_grd_df %>%
+  left_join(regions_df, by = "state") %>%
+  group_by(region, year) %>%
+  summarize(N = n(), Min = min(law_score), Max = max(law_score), AvgScore = mean(law_score),
+            Median = median(law_score), IQR = IQR(law_score), SD = sd(law_score))
+# Northeast displays greatest variation of scores by SD/IQR and highest median score by far.
+# Midwest shows least variation among scores by SD and a non-zero median.
+# South also exhibits minimal variation, in spite of largest N value, but at much lower scores.
+# West displays greater variation and a steadily rising mean score.
 
 # Check internal correlation between Giffords Law Rank and Death Rank
 # Source death rank reversed to maintain same good -> bad scale
@@ -41,10 +62,14 @@ giff_grd_df %>%
   geom_point() +
   stat_smooth(method = "lm", se = FALSE)
 
-# Calculate r2 and other summary statistics
-gf_mod1 <- lm(death_rnk ~ law_rnk, giff_grd_df)
-summary(gf_mod1)
-# Model indicates r2 of 0.55
+# Scatterplot suggest significant correlation between law rank and death rank.
+
+# Calculate r2
+giff_grd_df %>%
+  left_join(regions_df, by = "state") %>%
+  summarize(N = n(), r2 = cor(law_rnk, death_rnk)^2)
+
+# Calculated r2 of 0.555 supports moderate-strong relationship.
 
 
 # Add geographical labeling element to identify outliers, facet by year
@@ -55,6 +80,9 @@ giff_grd_df %>%
   geom_text() +
   stat_smooth(method = "lm", se = FALSE)
 
+# Clear distinction between regions jumps out from regression lines. 
+
+
 # Add regional facet to view correlation across regions
 giff_grd_df %>%
   left_join(regions_df, by = "state") %>%
@@ -63,70 +91,179 @@ giff_grd_df %>%
   geom_text() +
   stat_smooth(method = "lm", se = FALSE)
 
+# Added regional facet increases readability while still highlighting regional distinctions.
 
 
-gf_ne_df <- giff_grd_df %>%
+giff_grd_df %>%
   left_join(regions_df, by = "state") %>%
-  filter(reg_code == 1)
+  group_by(region) %>%
+  summarize(N = n(), r2 = cor(law_rnk, death_rnk)^2)
 
-gf_ne_mod1 <- lm(death_rnk ~ law_rnk, gf_ne_df)
-summary(gf_ne_mod1)
-# r2 = 0.327
+# Calculated r2 indicates stronger correlation for South (0.525) and West (0.744)
+# and weaker relationship for Northeast (0.327) and Midwest (0.338).
+# This suggests regional factors are influencing the relationship.
 
-# ----------------------------------------------------------
 
-gf_midwest_df <- giff_grd_df %>%
-  left_join(regions_df, by = "state") %>%
-  filter(reg_code == 2)
+# =======================================================================
+# 
+# Statistical Analysis - Ranking Data: Guns & Ammo vs Giffords Law Center
+# 
+# =======================================================================
 
-gf_midwest_mod1 <- lm(death_rnk ~ law_rnk, gf_midwest_df)
-summary(gf_midwest_mod1)
-# r2 = 0.338
-
-# ----------------------------------------------------------
-
-gf_south_df <- giff_grd_df %>%
-  left_join(regions_df, by = "state") %>%
-  filter(reg_code == 3)
-
-gf_south_mod1 <- lm(death_rnk ~ law_rnk, gf_south_df)
-summary(gf_south_mod1)
-# r2 = o.525
-
-# ----------------------------------------------------------
-
-gf_west_df <- giff_grd_df %>%
-  left_join(regions_df, by = "state") %>%
-  filter(reg_code == 4)
-
-gf_west_mod1 <- lm(death_rnk ~ law_rnk, gf_west_df)
-summary(gf_west_mod1)
-# r2 = 0.744
-
-# ---------------------------------------------------------
-
-gun_ammo_df %>%
-  filter(year == 2015) %>%
-  left_join(giff_grd_df, by = join_key) %>%
-  left_join(regions_df, by = "state") %>%
-  ggplot(aes(x = gun_ammo_rnk, y = law_rnk, label = usps_st, color = region)) +
-  geom_text(position = "jitter") +
-  stat_smooth(method = "lm", se = FALSE, colour = "blue")
-
-ga_gf_mod <- gun_ammo_df %>%
-  filter(year == 2015) %>%
-  left_join(giff_grd_df, by = join_key)
-  
-ga_gf_mod1 <- lm(law_rnk ~ gun_ammo_rnk, ga_gf_mod)
-summary(ga_gf_mod1) 
-
+# Check relationship between Guns & Ammo Rank and Giffords Death Rank
 gun_ammo_df %>%
   filter(year == 2015) %>%
   left_join(giff_grd_df, by = join_key) %>%
   left_join(regions_df, by = "state") %>%
   ggplot(aes(x = gun_ammo_rnk, y = death_rnk, label = usps_st, color = region)) +
   geom_text(position = "jitter") +
+  stat_smooth(method = "lm", se = FALSE) +
+  facet_grid(. ~ region)
+
+gun_ammo_df %>%
+  filter(year == 2015) %>%
+  left_join(giff_grd_df, by = join_key) %>%
+  summarize(N = n(), r2 = cor(death_rnk, gun_ammo_rnk)^2)
+
+# r2 = 0.409 indicating modearate but still significant negative relationship
+# between Guns & Ammo ranking of Best States for Gun Owners and the Giffords
+# death rank.
+
+# Run G&A Rank against CDC Firearm Homicide Rate
+gun_ammo_df %>%
+  filter(year == 2015) %>%
+  left_join(gun_deaths_df, by = join_key) %>%
+  left_join(regions_df, by = "state") %>%
+  ggplot(aes(x = gun_ammo_rnk, y = hom_rate, label = usps_st, color = region)) +
+  geom_text(position = "jitter") +
+  stat_smooth(method = "lm", se = FALSE)
+  
+gun_ammo_df %>%
+  filter(year == 2015) %>%
+  left_join(gun_deaths_df, by = join_key) %>%
+  summarize(N = n(), r2 = cor(hom_rate, gun_ammo_rnk)^2)
+
+# No apparent correlation between G&A rank and firearm homicide rate, r2 of 0.059
+
+# Run G&A Rank against CDC Firearm Suicide Rates
+gun_ammo_df %>%
+  filter(year == 2015) %>%
+  left_join(gun_deaths_df, by = join_key) %>%
+  left_join(regions_df, by = "state") %>%
+  ggplot(aes(x = gun_ammo_rnk, y = sui_rate, label = usps_st, color = region)) +
+  geom_text(position = "jitter") +
   stat_smooth(method = "lm", se = FALSE, colour = "blue")
 
-mod2 <- lm(death_rnk ~ gun_ammo_rnk, ga_gf_mod)
-summary(mod2) 
+gun_ammo_df %>%
+  filter(year == 2015) %>%
+  left_join(gun_deaths_df, by = join_key) %>%
+  summarize(N = n(), r2 = cor(sui_rate, gun_ammo_rnk)^2)
+
+# Significant correlation between G&A rank and firearm suicide rate, r2 of 0.466
+
+# Add regional facet to investigate geographical influences
+gun_ammo_df %>%
+  filter(year == 2015) %>%
+  left_join(gun_deaths_df, by = join_key) %>%
+  left_join(regions_df, by = "state") %>%
+  ggplot(aes(x = gun_ammo_rnk, y = sui_rate, label = usps_st, color = region)) +
+  geom_text(position = "jitter") +
+  stat_smooth(method = "lm", se = FALSE) +
+  facet_grid(. ~ region)
+
+# Sharp regional contrasts emerge once more
+
+# Run same graph as above but between Gifford Law Rank and Suicide Rate
+giff_grd_df %>%
+  filter(year == 2015) %>%
+  left_join(gun_deaths_df, by = join_key) %>%
+  left_join(regions_df, by = "state") %>%
+  ggplot(aes(x = law_rnk, y = sui_rate, label = usps_st, color = region)) +
+  geom_text(position = "jitter") +
+  stat_smooth(method = "lm", se = FALSE) +
+  facet_grid(. ~ region)
+
+giff_grd_df %>%
+  filter(year == 2015) %>%
+  left_join(gun_deaths_df, by = join_key) %>%
+  left_join(regions_df, by = "state") %>%
+  summarize(N = n(), r2 = cor(sui_rate, law_rnk)^2)
+
+# =======================================================================
+# 
+# Statistical Analysis - Gun Ownership Rates
+# 
+# =======================================================================
+
+# Run general statistical summary
+gun_own_2013_df %>%
+  summarize(N = n(), Min = min(own_rate), Max = max(own_rate), AvgScore = mean(own_rate), 
+            Median = median(own_rate), IQR = IQR(own_rate), SD = sd(own_rate))
+
+# AVerage ownership rate of 33% ranging from 5% to 61.7%
+
+# Add geographical layer to check ownership rate by region
+gun_own_2013_df %>%
+  left_join(regions_df, by = "state") %>%
+  group_by(region) %>%
+  summarize(N = n(), Min = min(own_rate), Max = max(own_rate), AvgScore = mean(own_rate), 
+            Median = median(own_rate), IQR = IQR(own_rate), SD = sd(own_rate))
+  
+# Below average rates in Northeast, above average in West and South - regional dynamic again.
+
+# Display regional difference w/ boxplots
+gun_own_2013_df %>%
+  left_join(regions_df, by = "state") %>%
+  ggplot(aes(x = region, y = own_rate, color = region, label = usps_st)) +
+  geom_boxplot() +
+  geom_text(position = "jitter")
+
+# Boxplot displays regional differences and outliers w/in regions
+
+# Ownership rates only available for 2013, will compare to Giffords 2014
+giff_grd_df %>%
+  filter(year == 2014) %>%
+  left_join(gun_own_2013_df, by = "state") %>%
+  left_join(regions_df, by = "state") %>%
+  ggplot(aes(x = own_rate, y = law_rnk, label = usps_st, color = region)) +
+  geom_text() +
+  stat_smooth(method = "lm", se = FALSE, colour = "blue")
+
+giff_grd_df %>%
+  filter(year == 2014) %>%
+  left_join(gun_own_2013_df, by = "state") %>%
+  summarize(N = n(), r2 = cor(own_rate, law_rnk)^2)
+
+# Moderate negative relationship between Gifford Law Rank and Ownership Rate
+# with an r2 of 0.393
+
+# Run ownership rate against suicide rate for 2013
+gun_deaths_df %>%
+  filter(year == 2013) %>%
+  left_join(gun_own_2013_df, by = "state") %>%
+  left_join(regions_df, by = "state") %>%
+  ggplot(aes(x = own_rate, y = sui_rate, label = usps_st, color = region)) +
+  geom_text() +
+  stat_smooth(method = "lm", se = FALSE, colour = "blue")
+
+gun_deaths_df %>%
+  filter(year == 2013) %>%
+  left_join(gun_own_2013_df, by = "state") %>%
+  ungroup(state) %>%                                 # Don't know why needed to ungroup, but it worked?
+  summarize(N = n(), r2 = cor(own_rate, sui_rate)^2)
+
+# r2 of 0.547 indicates solid relationship between suicide rate and gun ownership 
+
+# Add regional facet to highlights differences in ownership and suicide rates
+gun_deaths_df %>%
+  filter(year == 2013) %>%
+  left_join(gun_own_2013_df, by = "state") %>%
+  left_join(regions_df, by = "state") %>%
+  ggplot(aes(x = own_rate, y = sui_rate, label = usps_st, color = region)) +
+  geom_text() +
+  stat_smooth(method = "lm", se = FALSE) +
+  facet_grid(. ~ region)
+
+
+
+
