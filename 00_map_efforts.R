650933e2ed600df6b90f4d3bf1c5d10963183e12
datahoundz <jscotland@cox.net>
2018-03-15 17:07:01 -0700

    Working on color palettes

--- 00_map_efforts.R
+++ 00_map_efforts.R
@@ -1,7 +1,8 @@
-# Test run with hex maps
+## Test run with hex maps
 
-# https://gist.github.com/hrbrmstr/4efedf3b24f7da4e24d1 (source for code below)
-# https://gist.github.com/benmarwick/a8888f96728f32e4191f81e7e62d2736
+## Source code for map creation below
+## https://gist.github.com/hrbrmstr/4efedf3b24f7da4e24d1 
+## https://gist.github.com/benmarwick/a8888f96728f32e4191f81e7e62d2736
 
 library(rgdal)
 library(rgeos)
@@ -16,7 +17,7 @@ us <- readOGR("us_states_hexgrid.geojson")
 centers <- cbind.data.frame(data.frame(gCentroid(us, byid=TRUE), id=us@data$iso3166_2))
 us_map <- fortify(us, region="iso3166_2")
 
-# =================================================================
+## =================================================================
 
 ## My edits to original source code
 
@@ -33,7 +34,7 @@ map_data <- gun_deaths_df %>%
 summary(map_data)
 head(map_data)
 
-# =================================================================
+## =================================================================
 
 gg <- ggplot()
 
@@ -47,36 +48,46 @@ gg <- gg + geom_map(
   size = 0.5
 )
 
-gg <- gg + geom_map(data = map_data,            # set data source
+gg <- gg + geom_map(data = map_data,            ## set data source
                     map = us_map,
-                    aes(fill = subregion,        # set map variable 
-                        map_id = usps_st))
+                    aes(fill = law_score,       #### set map variable 
+                        map_id = usps_st))      ## set map label data
 
 gg <- gg + geom_map(
-  data = map_data,                              # set data source again
+  data = map_data,                              ## set data source again
   map = us_map,
-  aes(map_id = usps_st),                        # set map label data
+  aes(map_id = usps_st),                        ## set map label data
   fill = "#ffffff",
   alpha = 0,
   color = "white",
   show.legend = FALSE
 ) 
 
+library(RColorBrewer)
+myBlues = brewer.pal(n = 9, "Blues")[3:9]
+pal = colorRampPalette(myBlues)
+
 gg <- gg + geom_text(data=centers, aes(label=id, x=x, y=y), color="white", size=4)
 
 gg <- gg + coord_map() +
   theme_bw() +
-  # scale_fill_continuous(trans = "reverse") +
-  scale_fill_discrete() +
+  # scale_fill_brewer(palette = "Blues") +
+  scale_fill_continuous(low = "#c6dbef", high = "#08306b") +
   xlab("") +
-  ylab("")
+  ylab("") +
+  labs(fill = "Law Score") +
+  labs(title = "Giffords Law Center: State Gun Law Grades", 
+       subtitle = "Score: 0 = F, 4 = A") +
+  labs(caption = "Source: Giffords Law Center") 
 
 gg <- gg + theme(panel.border=element_blank())
 gg <- gg + theme(panel.spacing=unit(3, "lines"))
 gg <- gg + theme(panel.grid=element_blank())
 gg <- gg + theme(axis.ticks=element_blank())
 gg <- gg + theme(axis.text=element_blank())
+gg <- gg + theme(legend.position = "bottom")
 
-gg
+mp_giff_grd <- gg
+mp_giff_grd
 
   
