1a2d020dcb5e526559932e4b9f38b4a87a12b493
datahoundz <jscotland@cox.net>
2018-03-20 12:20:38 -0700

    Tested pop_density on linear regression model

--- 04_mach_learn.R
+++ 04_mach_learn.R
@@ -5,8 +5,10 @@
 
 mach_data_df <- sui_method_df %>%
   filter(state != "District of Columbia") %>%
+  left_join(population_df, by = join_key) %>%
+  select(-land_area, -pop.y, -pop.x) %>%
   left_join(regions_df, by = "state") %>%
-  select(year, state, usps_st, region, subregion, reg_code, subreg_code, gun_rate, other_rate, all_rate) %>%
+  select(year, state, usps_st, region, subregion, reg_code, subreg_code, gun_rate, other_rate, all_rate, pop_density) %>%
   left_join(state_laws_total_df, by = join_key) %>%
   left_join(laws_cat_df, by = join_key) %>%
   left_join(gun_own_2013_df, by = "state") %>%
@@ -21,10 +23,11 @@ mach_data_df$subreg_code <- factor(mach_data_df$subreg_code)
 mach_data_df <- mach_data_df %>%
   mutate(reg_west = reg_code == 4) 
 
+
 View(mach_data_df)  
 
 # Run correlation matrix on prospective variables
-mach_data_df[ , 8:27] %>%
+mach_data_df[ , 8:28] %>%
   cor()
 
 # Run single variable linear model
@@ -32,10 +35,15 @@ mod1 <- lm(gun_rate ~ own_rate, mach_data_df)
 summary(mod1)
 
 # Run multi-variate linear model
-mod2 <- lm(gun_rate ~ own_rate + buy_reg + reg_code, mach_data_df)
+mod2 <- lm(gun_rate ~ reg_code + own_rate + buy_reg, mach_data_df)
 summary(mod2)
-
 
 augment(mod2)
 confint(mod2)
 anova(mod2)
+
+# Residual & Q-Q Plot code from linear regression exercise
+par(mar = c(4, 4, 2, 2), mfrow = c(1, 2)) #optional
+plot(mod2, which = c(1, 2)) # "which" argument optional
+
+mach_data_df[c(896, 648, 467), ]
