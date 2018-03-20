9945fa390b855440095acc21ea0744a5bcee4dbe
datahoundz <jscotland@cox.net>
2018-03-20 10:54:24 -0700

    More work on linear model plan

--- 04_mach_learn.R
+++ 04_mach_learn.R
@@ -6,20 +6,36 @@
 mach_data_df <- sui_method_df %>%
   filter(state != "District of Columbia") %>%
   left_join(regions_df, by = "state") %>%
-  select(state, usps_st, region, reg_code, subregion, subreg_code, year, gun_rate, other_rate, all_rate) %>%
-  left_join(laws_cat_df, by = join_key) %>%
+  select(year, state, usps_st, region, subregion, reg_code, subreg_code, gun_rate, other_rate, all_rate) %>%
   left_join(state_laws_total_df, by = join_key) %>%
+  left_join(laws_cat_df, by = join_key) %>%
   left_join(gun_own_2013_df, by = "state") %>%
   filter(year >= 1999)
 
+# Convert region/subregion to factor for linear regression
+mach_data_df$reg_code <- factor(mach_data_df$reg_code)
+mach_data_df$subreg_code <- factor(mach_data_df$subreg_code)
+
+# Create variable for reg_west T/F per feedback from initial model
+# Deciding to stick with reg_code, though reg_west exhibits predictive value
+mach_data_df <- mach_data_df %>%
+  mutate(reg_west = reg_code == 4) 
+
 View(mach_data_df)  
 
+# Run correlation matrix on prospective variables
+mach_data_df[ , 8:27] %>%
+  cor()
+
+# Run single variable linear model
 mod1 <- lm(gun_rate ~ own_rate, mach_data_df)
 summary(mod1)
 
-mod2 <- lm(gun_rate ~ own_rate + buy_reg, mach_data_df)
+# Run multi-variate linear model
+mod2 <- lm(gun_rate ~ own_rate + buy_reg + reg_code, mach_data_df)
 summary(mod2)
-augment(mod2)
 
-mach_data_df[ , 8:26] %>%
-  cor()
+
+augment(mod2)
+confint(mod2)
+anova(mod2)
